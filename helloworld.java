22c3f85b1aa3fab3ba869420d2e9710aa62502ac
veeru <veeru.nrt@gmail.com>
2019-06-01 06:12:04 -0400

    add some message to helloworld program

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,10 @@
 public class HelloWorld
-{   
+//welcome to the HelloWorld Java program//
+{//this is the start of the java program//
+   
       public static void main(String[] args)
       {       
          System.out.println("Hello gist @ GitHub!");   
-      } // end of the helloworld process//
+      }
+ // end of the helloworld process//
 } 
