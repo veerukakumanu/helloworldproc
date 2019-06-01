5b76a328c98d753b3bf61a8e67483c8a4234e610
veeru <veeru.nrt@gmail.com>
2019-06-01 06:31:00 -0400

    new changes to the helloworld program

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,8 @@
 public class HelloWorld
-//welcome to the HelloWorld Java program//
-{//this is the start of the java program//
-   
+{// enter into helloworld program//
+ //start the helloworld program//   
       public static void main(String[] args)
       {       
          System.out.println("Hello gist @ GitHub!");   
-      }
- // end of the helloworld process//
+      } // end of the helloworld process//
 } 
